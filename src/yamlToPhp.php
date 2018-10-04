65b87664ebef9f5a1c266e70a27f44d7c15b5700
LucioTrincheri <lucio4852071@gmail.com>
2018-10-04 13:17:57 -0300

    Muevo el template a su propio directorio y modifico archivos

--- src/yamlToPhp.php
+++ src/yamlToPhp.php
@@ -4,19 +4,31 @@ namespace GEOM;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+//Cargo Yaml
 use Symfony\Component\Yaml\Yaml;
-
 $yaml = Yaml::parseFile('example/preguntas.yml');
-
 $yaml = $yaml['preguntas'];
 
 //Transformo el array en clases 'Pregunta'
-	
 $preguntas = [];
 $leng = count($yaml);
 for($i=0; $i<$leng ;$i++)
 	$preguntas[$i] = new Pregunta($yaml[$i]);
-//Fin Transformacion del array
 
-file_put_contents('output.txt', print_r($yaml, true));
+
+function mezclarTests(){
+	$leng = count($preguntas);
+	for($i=0; $i<$leng ;$i++)
+		$preguntas[$i]->shuffleAnswers();
+	shuffle($preguntas);
+}
+
+//Carga de Twig
+/*
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+$template = $twig->load('index.html');
+//Render del HTML con las variables
+echo $template->render(/*array('the' => 'variables', 'go' => 'here'));
+*/
 ?>
